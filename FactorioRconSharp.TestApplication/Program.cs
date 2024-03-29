f38b92c6a814275368b59e29587c267e0fdaed63
Ismail Bennani <lahkim.bennani.ismail@gmail.com>
2024-03-29 13:05:01 +0100

    udpate readme

--- FactorioRconSharp.TestApplication/Program.cs
+++ FactorioRconSharp.TestApplication/Program.cs
@@ -1,28 +1,10 @@
-﻿using System.Linq.Expressions;
-using FactorioRconSharp;
-using FactorioRconSharp.Core.Visitor;
-using FactorioRconSharp.Model;
+﻿using FactorioRconSharp;
 
 using FactorioRconClient client = new("127.0.0.1", 27015);
 bool connected = await client.ConnectAsync("password");
 
 Console.WriteLine("Connected: " + connected);
 
-Console.WriteLine("Game:");
+string mapString = await client.ReadAsync(g => g.Game.GetMapExchangeString());
 
-await Log(g => g.Game.IsDemo());
-await Log(g => g.Game.IsMultiplayer());
-await Log(g => g.Game.GetMapExchangeString());
-await Log(g => g.Game.GetActiveEntitiesCount(null));
-await Log(g => g.Game.GetActiveEntitiesCount(1));
-await Log(g => g.Game.GetActiveEntitiesCount("nauvis"));
-
-return;
-
-async Task Log<T>(Expression<Func<FactorioRconGlobals, T>> func)
-{
-    string strExpression = new FactorioRconTranslator().BuildExpression(func);
-    T result = await client.ReadAsync(func);
-
-    Console.WriteLine($"\t{strExpression}: {result}");
-}
+Console.WriteLine($"Map string: {mapString}");
