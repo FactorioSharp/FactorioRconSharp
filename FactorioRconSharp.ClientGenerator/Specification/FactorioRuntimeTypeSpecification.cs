dfd6ec2102823082c7ca540add1514991185f549
Ismail Bennani <lahkim.bennani.ismail@gmail.com>
2024-03-29 12:23:07 +0100

    remove warning

--- FactorioRconSharp.ClientGenerator/Specification/FactorioRuntimeTypeSpecification.cs
+++ FactorioRconSharp.ClientGenerator/Specification/FactorioRuntimeTypeSpecification.cs
@@ -1,10 +1,13 @@
-﻿namespace FactorioRconSharp.ClientGenerator.Specification;
+﻿using System.Runtime.CompilerServices;
+
+namespace FactorioRconSharp.ClientGenerator.Specification;
 
 public abstract class FactorioRuntimeTypeSpecification : IEquatable<FactorioRuntimeTypeSpecification>
 {
     public abstract bool Equals(FactorioRuntimeTypeSpecification? other);
 
     public override bool Equals(object? obj) => Equals(obj as FactorioRuntimeTypeSpecification);
+    public override int GetHashCode() => RuntimeHelpers.GetHashCode(this);
 
     public static bool operator ==(FactorioRuntimeTypeSpecification? left, FactorioRuntimeTypeSpecification? right) => Equals(left, right);
 
