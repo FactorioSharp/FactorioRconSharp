d189168af643e6f4eac8be1160c67f6dffd13460
Ismail Bennani <lahkim.bennani.ismail@gmail.com>
2024-03-30 18:17:43 +0100

    write dummy parser method

---
+++ SpecificationCompiler/Model/Writers/FactorioModelDocumentationWriterExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System.Text.RegularExpressions;
+using CaseExtensions;
+
+namespace SpecificationCompiler.Model.Writers;
+
+static partial class FactorioModelDocumentationWriterExtensions
+{
+    public static async Task WriteAsync(this FactorioModelDocumentation documentation, TextWriter writer, int indentLevel = 0)
+    {
+        if (!string.IsNullOrWhiteSpace(documentation.Summary))
+        {
+            await writer.WriteDocumentationLineAsync("<summary>", indentLevel);
+            await writer.WriteDocumentationLineAsync($"{ReplaceDocumentationLinks(documentation.Summary)}", indentLevel);
+            await writer.WriteDocumentationLineAsync("</summary>", indentLevel);
+        }
+
+        if (!string.IsNullOrWhiteSpace(documentation.Remarks))
+        {
+            await writer.WriteDocumentationLineAsync("<remarks>", indentLevel);
+            await writer.WriteDocumentationLineAsync($"{ReplaceDocumentationLinks(documentation.Remarks)}", indentLevel);
+            await writer.WriteDocumentationLineAsync("</remarks>", indentLevel);
+        }
+
+        if (!string.IsNullOrWhiteSpace(documentation.Examples))
+        {
+            await writer.WriteDocumentationLineAsync("<examples>", indentLevel);
+            await writer.WriteDocumentationLineAsync($"{ReplaceDocumentationLinks(documentation.Examples)}", indentLevel);
+            await writer.WriteDocumentationLineAsync("</examples>", indentLevel);
+        }
+    }
+
+    static string ReplaceDocumentationLinks(string documentation)
+    {
+        Regex linkRegex = DocumentationLinkRegex();
+
+        MatchCollection matches = linkRegex.Matches(documentation);
+        foreach (Match match in matches.ToArray())
+        {
+            string symbol = match.Groups["symbol"].Value;
+            string sharpSymbol = string.Join(".", symbol.Split("::").Select(s => s.ToPascalCase()));
+            documentation = documentation.Replace(match.Value, $"<see cref=\"{sharpSymbol}\" />");
+        }
+
+        return documentation;
+    }
+
+    [GeneratedRegex(@"\[.*\]\(runtime:(?<symbol>.*)\)", RegexOptions.Multiline | RegexOptions.Compiled)]
+    private static partial Regex DocumentationLinkRegex();
+}
---
+++ SpecificationCompiler/Model/Writers/FactorioModelParserWriter.cs
@@ -0,0 +1,7 @@
+﻿namespace SpecificationCompiler.Model.Writers;
+
+static class FactorioModelParserWriter
+{
+    public static async Task WriteStaticParserMethodAsync(TextWriter writer, FactorioModelClass cls, int indentLevel = 0) =>
+        await writer.WriteLineAsync($"public abstract {cls.Name} FromCommandResult(string str);", indentLevel);
+}
