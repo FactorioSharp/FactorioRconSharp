902443d3c041f27611079df7c1d49c4d355231d1
Ismail Bennani <lahkim.bennani.ismail@gmail.com>
2024-04-04 21:21:36 +0200

    Expose low level client (#44)
    expose low level client

--- FactorioSharp.Rcon/FactorioRconClientBase.cs
+++ FactorioSharp.Rcon/FactorioRconClientBase.cs
@@ -8,35 +8,35 @@ namespace FactorioSharp.Rcon;
 
 public class FactorioRconClient : IDisposable
 {
-    readonly FactorioRemoteConsole _lowLevelClient;
+    public FactorioRemoteConsole LowLevelClient { get; }
 
-    public bool Silent { get => _lowLevelClient.Silent; set => _lowLevelClient.Silent = value; }
-    public bool Connected => _lowLevelClient.Connected;
+    public bool Silent { get => LowLevelClient.Silent; set => LowLevelClient.Silent = value; }
+    public bool Connected => LowLevelClient.Connected;
 
     public FactorioRconClient(string ipAddress, int port)
     {
-        _lowLevelClient = new FactorioRemoteConsole(ipAddress, port);
+        LowLevelClient = new FactorioRemoteConsole(ipAddress, port);
     }
 
-    public async Task<bool> ConnectAsync(string password) => await _lowLevelClient.ConnectAsync(password);
-    public void Disconnect() => _lowLevelClient.Disconnect();
+    public async Task<bool> ConnectAsync(string password) => await LowLevelClient.ConnectAsync(password);
+    public void Disconnect() => LowLevelClient.Disconnect();
 
     public async Task ExecuteAsync(Expression<Action<FactorioRconGlobals>> action)
     {
         string expression = new FactorioRconTranslator().BuildExpression(action);
-        await _lowLevelClient.ExecuteAsync(expression);
+        await LowLevelClient.ExecuteAsync(expression);
     }
 
     public async Task ExecuteAsync<TArg>(Expression<Action<FactorioRconGlobals, TArg>> action, TArg arg)
     {
         string expression = new FactorioRconTranslator(arg).BuildExpression(action);
-        await _lowLevelClient.ExecuteAsync(expression);
+        await LowLevelClient.ExecuteAsync(expression);
     }
 
     public async Task ExecuteAsync(Expression<Action<FactorioRconGlobals, Dictionary<string, object>>> action, Dictionary<string, object> ctx)
     {
         string expression = new FactorioRconTranslator(ctx).BuildExpression(action);
-        await _lowLevelClient.ExecuteAsync(expression);
+        await LowLevelClient.ExecuteAsync(expression);
     }
 
     public async Task<T?> ReadAsync<T>(Expression<Func<FactorioRconGlobals, T>> func)
@@ -59,7 +59,7 @@ public class FactorioRconClient : IDisposable
 
     public void Dispose()
     {
-        _lowLevelClient.Dispose();
+        LowLevelClient.Dispose();
         GC.SuppressFinalize(this);
     }
 
@@ -69,15 +69,15 @@ public class FactorioRconClient : IDisposable
         Type type = typeof(T);
         if (type.IsTableType())
         {
-            strResult = await _lowLevelClient.ReadAsync($"game.table_to_json({expression})");
+            strResult = await LowLevelClient.ReadAsync($"game.table_to_json({expression})");
         }
         else if (type.IsLuaCustomTable())
         {
-            strResult = await _lowLevelClient.ExecuteAsync($"result = {{}}; for k, v in pairs({expression}) do result[k] = v; end; rcon.print(game.table_to_json(result))");
+            strResult = await LowLevelClient.ExecuteAsync($"result = {{}}; for k, v in pairs({expression}) do result[k] = v; end; rcon.print(game.table_to_json(result))");
         }
         else
         {
-            strResult = await _lowLevelClient.ReadAsync(expression);
+            strResult = await LowLevelClient.ReadAsync(expression);
         }
 
         return FactorioRconParser.Parse<T>(strResult.Trim());
